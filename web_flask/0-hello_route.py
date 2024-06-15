94f271c6af11035906ac23a3491add324025ae0e
Aosama <ahmed.osama@elsewedyintel.com>
2024-06-16 01:38:00 +0300

    fix routes

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
- start Flash web App listen on 0.0.0.0 port 5000
+ start Flask web App listen on 0.0.0.0 port 5000
 """
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def index():
+    """return Hello HBNB! string"""
+    return 'Hello HBNB!'
+
 if __name__ == '__main__':
-    from flask import Flask
-    app = Flask(__name__)
-
-    @app.route('/', strict_slashes=False)
-    def hello_hbnb():
-        """
-        display "Hello HBNB!"
-        """
-        return "Hello HBNB!"
-
     app.run(host='0.0.0.0', port='5000')
